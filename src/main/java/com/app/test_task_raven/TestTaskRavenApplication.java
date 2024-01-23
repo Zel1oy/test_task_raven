b64357625468b37a9c0fc015a5a8cf09ff29b18c
Arsen Mehdalskyi <arsen25public@gmail.com>
2024-01-23 16:23:50 +0200

    first commit

---
+++ src/main/java/com/app/test_task_raven/TestTaskRavenApplication.java
@@ -0,0 +1,13 @@
+package com.app.test_task_raven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskRavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTaskRavenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/test_task_raven/TestTaskRavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.test_task_raven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskRavenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
